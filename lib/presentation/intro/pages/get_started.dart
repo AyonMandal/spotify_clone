bbb813284a64f4319e437d95f9d6c46f5ab034ba
Ayon Mandal <ayon@urban.health>
2024-11-05 17:42:54 +0530

    added auto redirect with sublime animation

---
+++ lib/presentation/intro/pages/get_started.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class GetStarted extends StatelessWidget {
+  const GetStarted({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Get Started'),
+      ),
+    );
+  }
+}
--- lib/presentation/splash/page/splash.dart
+++ lib/presentation/splash/page/splash.dart
@@ -1,15 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spotify_dummy/core/config/assets/app_vectors.dart';
+import 'package:spotify_dummy/presentation/intro/pages/get_started.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  double _opacity = 1.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _startFadeOutAndRedirect();
+  }
+
+  Future<void> _startFadeOutAndRedirect() async {
+    await Future.delayed(const Duration(seconds: 2));
+    setState(() {
+      _opacity = 0.0;
+    });
+    await Future.delayed(const Duration(seconds: 1));
+    if (mounted) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const GetStarted(),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SvgPicture.asset(AppVectors.spotifyLogo),
+        child: AnimatedOpacity(
+          opacity: _opacity,
+          duration: const Duration(seconds: 1),
+          child: SvgPicture.asset(AppVectors.spotifyLogo),
+        ),
       ),
     );
   }
